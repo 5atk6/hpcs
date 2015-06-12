c3e1c700cadf843fc47805d67a7c7156e4fdcdfd
Atsuki IWAI <s1311335@pentas-compd.(none)>
2015-06-12 16:09:06 +0900

    probably MPI Finish

--- subject4/gravityMPI.c
+++ subject4/gravityMPI.c
@@ -15,7 +15,7 @@
 #define step 10
 
 int main(int argc,char* argv[]){
-  double r,axij,ayij,azij,*m,*x,*y,*z,*vx,*vy,*vz,*vxiNext,*vyiNext,*vziNext,*xiNext,*yiNext,*ziNext,startTime,endTime,time,*sendXbuf,*sendYbuf,*sendZbuf,*receiveXbuf,*receiveYbuf,*receiveZbuf,*global_m,*global_x,*global_y,*global_z,*global_vx,*global_vy,*global_vz,*local_m,*local_x,*local_y,*local_z,*local_vx,*local_vy,*local_vz;
+  double r,axij,ayij,azij,*m,*x,*y,*z,*vx,*vy,*vz,*vxiNext,*vyiNext,*vziNext,*xiNext,*yiNext,*ziNext,startTime,endTime,local_time,*global_m,*global_x,*global_y,*global_z,*global_vx,*global_vy,*global_vz,*local_m,*local_x,*local_y,*local_z,*local_vx,*local_vy,*local_vz;
   int i,j,k,t;
 
   MPI_Status recv_status;
@@ -37,7 +37,6 @@ int main(int argc,char* argv[]){
   local_vy=(double*)malloc(sizeof(double)*size/nprocs);
   local_vz=(double*)malloc(sizeof(double)*size/nprocs);
   
-  
   //データを読み込む
   if(my_rank==0){  
     global_m=(double*)malloc(sizeof(double)*size);
@@ -55,8 +54,10 @@ int main(int argc,char* argv[]){
     read_data(argv[5],global_vx,size);
     read_data(argv[6],global_vy,size);
     read_data(argv[7],global_vz,size);
-  printf("hogeee %lf\n",global_x[3]);  
+    //printf("hogeee %lf\n",global_x[500]);  
   }
+  MPI_Barrier(MPI_COMM_WORLD);
+  //他プロセスにランク0で読み込んだデータを分配
   MPI_Scatter(global_m,size/nprocs,MPI_DOUBLE,local_m,size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
   MPI_Scatter(global_x,size/nprocs,MPI_DOUBLE,local_x,size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
   MPI_Scatter(global_y,size/nprocs,MPI_DOUBLE,local_y,size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
@@ -64,8 +65,8 @@ int main(int argc,char* argv[]){
   MPI_Scatter(global_vx,size/nprocs,MPI_DOUBLE,local_vx,size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
   MPI_Scatter(global_vy,size/nprocs,MPI_DOUBLE,local_vy,size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
   MPI_Scatter(global_vz,size/nprocs,MPI_DOUBLE,local_vz,size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
-  MPI_Barrier(MPI_COMM_WORLD);
-  printf("ここまではおっけー %d %lf\n",my_rank,local_x[3]);      		      
+  //printf("hoge %d %lf\n",my_rank,local_x[0]);
+  
   vxiNext=(double*)malloc(sizeof(double)*size/nprocs);
   vyiNext=(double*)malloc(sizeof(double)*size/nprocs);
   vziNext=(double*)malloc(sizeof(double)*size/nprocs);
@@ -74,7 +75,7 @@ int main(int argc,char* argv[]){
   ziNext=(double*)malloc(sizeof(double)*size/nprocs);
   
   //初期化
-  for(i=0;i<size;i++){
+  for(i=0;i<size/nprocs;i++){
     vxiNext[i]=0;
     vyiNext[i]=0;
     vziNext[i]=0;
@@ -85,35 +86,37 @@ int main(int argc,char* argv[]){
   
   
   //時間計測について
-  time=0.0;
+  local_time=0.0;
     
   double global_axij=0;
   double global_ayij=0;
   double global_azij=0;
   //重力の計算 
   for(t=0;t<step;t++){
-    
+    MPI_Barrier(MPI_COMM_WORLD);
     startTime=MPI_Wtime();
-      
-    for(i=my_rank*(size/nprocs);i<my_rank*(size/nprocs)+(size/nprocs);i++){
+
+    for(i=0;i<size/nprocs;i++){
+    //for(i=my_rank*(size/nprocs);i<my_rank*(size/nprocs)+(size/nprocs);i++){
       //aijについて
       axij=0; ayij=0; azij=0; //aijの初期化
       for(j=0;j<size;j++){
       if(i==j){
 	  continue;
       }
-	
+      
 	r=sqrt((local_x[i]-local_x[j])*(local_x[i]-local_x[j])+(local_y[i]-local_y[j])*(local_y[i]-local_y[j])+(local_z[i]-local_z[j])*(local_z[i]-local_z[j]));
 	axij+=G*(local_m[j]/(r*r))*((local_x[j]-local_x[i])/r);
 	ayij+=G*(local_m[j]/(r*r))*((local_y[j]-local_y[i])/r);
 	azij+=G*(local_m[j]/(r*r))*((local_z[j]-local_z[i])/r);
       }
-      //MPI_Barrier(MPI_COMM_WORLD);
+      MPI_Barrier(MPI_COMM_WORLD);
       
       //他プロセスのaijの情報を受け取り足し合わせてrxijに入れる
-      MPI_Reduce(&axij,&global_axij,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
-      //MPI_Allreduce(&ayij,&global_ayij,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
-      //MPI_Allreduce(&azij,&global_azij,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+      MPI_Allreduce(&axij,&global_axij,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+      MPI_Allreduce(&ayij,&global_ayij,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+      MPI_Allreduce(&azij,&global_azij,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+      //printf("ここまではおっけー %d \n",my_rank);
       
       vxiNext[i]=local_vx[i]+global_axij; 
       vyiNext[i]=local_vy[i]+global_axij; 
@@ -123,18 +126,19 @@ int main(int argc,char* argv[]){
       yiNext[i]=local_y[i]+vyiNext[i]*dt;
       ziNext[i]=local_z[i]+vziNext[i]*dt;
     }
+    MPI_Barrier(MPI_COMM_WORLD);
     endTime=MPI_Wtime();
-    time+=endTime-startTime;
+    local_time+=endTime-startTime;
     
-    memcpy(x,xiNext,sizeof(x)*size/nprocs);
-    memcpy(y,yiNext,sizeof(y)*size/nprocs);
-    memcpy(z,ziNext,sizeof(z)*size/nprocs);
-    memcpy(vx,vxiNext,sizeof(vx)*size/nprocs);
-    memcpy(vy,vyiNext,sizeof(vy)*size/nprocs);
-    memcpy(vz,vziNext,sizeof(vz)*size/nprocs);
+    memcpy(local_x,xiNext,sizeof(x)*size/nprocs);
+    memcpy(local_y,yiNext,sizeof(y)*size/nprocs);
+    memcpy(local_z,ziNext,sizeof(z)*size/nprocs);
+    memcpy(local_vx,vxiNext,sizeof(vx)*size/nprocs);
+    memcpy(local_vy,vyiNext,sizeof(vy)*size/nprocs);
+    memcpy(local_vz,vziNext,sizeof(vz)*size/nprocs);
     
   }
-  printf("time = %lf\n",time/step);
+  printf("time = %lf\n",local_time/step);
   
   /*write_data(x,"xex.dat",size);
   write_data(y,"yex.dat",size);
@@ -142,21 +146,31 @@ int main(int argc,char* argv[]){
   write_data(vxiNext,"vxex.dat",size);
   write_data(vyiNext,"vyex.dat",size);
   write_data(vxiNext,"vzex.dat",size);*/
-  
-  free(m);
-  free(x);
-  free(y);
-  free(z);
-  free(vx);
-  free(vy);
-  free(vz);
+
+  if(my_rank==0){
+    free(global_m);
+    free(global_x);
+    free(global_y);
+    free(global_z);
+    free(global_vx);
+    free(global_vy);
+    free(global_vz);
+  }
+
+  free(local_m);
+  free(local_x);
+  free(local_y);
+  free(local_z);
+  free(local_vx);
+  free(local_vy);
+  free(local_vz);
   free(xiNext);
   free(yiNext);
   free(ziNext);
   free(vxiNext);
   free(vyiNext);
   free(vziNext);
-
+  
   printf("success\n");
   MPI_Finalize();
   
